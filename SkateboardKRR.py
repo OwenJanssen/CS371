567c3b3772a19431bd6fe8f49afe6da7a28bbfee
jack-wheeler <107000677+jack-wheeler@users.noreply.github.com>
2023-02-17 16:12:21 -0600

    Add files via upload

--- SkateboardKRR.py
+++ SkateboardKRR.py
@@ -6,10 +6,16 @@ script_path = os.path.abspath(__file__)
 script_dir = os.path.dirname(script_path)
 os.chdir(script_dir)
 tricks = pd.read_csv("Skateboarding Tricks.csv")
+from typing import Any
+from logging import getLogger, DEBUG, INFO
+
+
 
 # ACTUAL CODE
 # Import companionsKQML package
 from companionsKQML import *
+
+LOGGER = getLogger(__name__)
 
 # Define a class for your Pythonian agent
 class MyAgent(Pythonian):
@@ -18,19 +24,29 @@ class MyAgent(Pythonian):
     def __init__(self, **kwargs):
         # Call the parent constructor with your agent's name
         super().__init__(**kwargs)
+        if self.debug:
+            LOGGER.setLevel(DEBUG)
+        else:
+            LOGGER.setLevel(INFO)
 
         # Add some facts using insert performative
-        self.insert(Expression(["isa", "John", "Person"]))
-        self.insert(Expression(["likes", "John", "Pizza"]))
+        self.send(listify(["isa", "John", "Person"]))
+        self.send(listify(["likes", "John", "Pizza"]))
 
         # Do some reasoning using ask_one performative
-        answer = self.ask_one(Expression(["likes", "?x", "Pizza"]))
+        answer = self.add_ask(self.test_ask_return_list1)
         
         # Print out the answer
-        print(answer)
+        #print(answer)
+
+    @staticmethod
+    def test_ask_return_list1(_input: Any):
+        
+       
+        LOGGER.info('testing ask with _input %s', _input)
+        return [_input]
 
 # Use convenience function to create an instance of your agent 
-agent = MyAgent.init_check_companions()
-
+agent = MyAgent(host='localhost', port=9100)
 # Start listening for KQML messages from Companion 
-agent.listen()
+agent.connect()
